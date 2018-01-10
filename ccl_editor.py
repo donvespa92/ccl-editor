e9133fa81fb9d3baa8741ce754886d20e3c50057
Bence Darázs <bence.darazs@econ.local>
2018-01-10 15:29:35 +0100

    Import objects not finished

--- ccl_editor.py
+++ ccl_editor.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import re
+from tkinter import ttk
 from tkinter import filedialog
 from tkinter.font import Font
 from tkinter import messagebox
@@ -13,9 +15,12 @@ class MainApplication(tk.Frame):
         self.master = master
         self.master.bind('<Escape>',self.cmd_escape)
         self.master.bind('<Control-f>',self.cmd_search)
+        self.object_types = ['Domain','Boundary','Domain Interface','Expressions','Material']
+        self.selected_objects = []
         
         self.gui_set_frames()
         self.gui_set_text()
+        self.gui_set_combobox()
         self.gui_set_searchbar()
         self.gui_set_menu()
         self.gui_set_grid()
@@ -30,7 +35,7 @@ class MainApplication(tk.Frame):
         self.text_output = tk.Text(
                 self.mainframe,
                 height=20,
-                width=60,
+                width=80,
                 wrap='none',
                 font='Consolas 11',
                 state='disabled')
@@ -43,12 +48,35 @@ class MainApplication(tk.Frame):
      
     def gui_set_searchbar(self):
         self.entry_searchbar = tk.Entry(self.frame_search,font=self.font,width=50)
-        
+     
+    def gui_set_combobox(self):
+        self.label_object_type = tk.Label(self.mainframe,text='Object type')
+        self.cbox_object_type = ttk.Combobox(
+                self.mainframe,
+                values=self.object_types,
+                state='readonly')
+        self.label_object_name = tk.Label(self.mainframe,text='Object name')
+        self.cbox_object_name = ttk.Combobox(
+                self.mainframe,
+                values=self.selected_objects,
+                state='readonly')
+        self.cbox_object_type.bind("<<ComboboxSelected>>", self.cmd_select_object_type)
+        self.cbox_object_type.set(self.object_types[0])
+   
     def gui_set_grid(self):
         self.mainframe.pack(fill='both',expand=1,padx=5,pady=5)
-        self.text_output.grid(row=1,column=1,sticky='NSEW')
-        self.text_xscrollbar.grid(row=2,column=1,sticky='NSEW')
-        self.text_yscrollbar.grid(row=1,column=2,sticky='NSEW')
+        self.mainframe.columnconfigure(1,weight=1)
+        self.mainframe.columnconfigure(2,weight=1)
+        self.mainframe.rowconfigure(2,weight=1)
+        
+        self.label_object_type.grid(row=0,column=1,sticky='W')
+        self.label_object_name.grid(row=0,column=2,sticky='W')
+        self.cbox_object_type.grid(row=1,column=1,sticky='NSEW')
+        self.cbox_object_name.grid(row=1,column=2,sticky='NSEW')
+        
+        self.text_output.grid(row=2,columnspan=2,column=1,sticky='NSEW')
+        self.text_xscrollbar.grid(row=3,columnspan=2,column=1,sticky='NSEW')
+        self.text_yscrollbar.grid(row=2,column=3,sticky='NSEW')
     
     def gui_set_menu(self):
         self.menubar = tk.Menu(self.master)
@@ -69,7 +97,57 @@ class MainApplication(tk.Frame):
         self.menubar.add_cascade(label='Edit',menu=self.editmenu)
                     
     def cmd_open_file(self):
+        temp = tk.filedialog.askopenfilename(
+                title='Choose a .def file',
+                filetypes=(              
+                        ("CFX Command file", "*.ccl"),
+                        ("Solver input file", "*.def"),
+                        ("Result file", "*.res"),
+                        ("All files", "*.*") ) )
+        
+        if temp:
+            self.inputfile_fullpath = temp
+            self.inputfile_name = os.path.basename(self.inputfile_fullpath)
+            self.inputfile_dir_name = os.path.dirname(self.inputfile_fullpath)
+            self.inputfile_type = os.path.splitext(self.inputfile_fullpath)[1]
+            
+            self.get_obj_data()
+        else:
+            return
+               
+    def get_obj_data(self):
+        if os.path.exists('temp.ccl'):
+            os.remove('temp.ccl')
+        if self.inputfile_type == '.ccl':
+            self.get_obj_names(self.inputfile_fullpath)
+        else:
+            os.system('cfx5dfile -read-cmds %s -output %s' % (self.inputfile_fullpath,'temp.ccl'));
+            self.get_obj_names('temp.ccl')
+            
+    def get_obj_names(self,inputfile):
+        self.objects = {}
+        self.orig_setup = []
+        with open(inputfile) as fp:
+            for line in fp:
+                self.orig_setup.append(line.rstrip())
+        
+        object_dict = {'Domain':2,
+                       'Boundary':4,
+                       'Domain Interface':4}
+        
+        for obj in object_dict:
+            objects_found = []
+            for line in self.orig_setup:
+                if (obj.upper()+':' in line and re.search('[\s{%s}]' % object_dict[obj],line)):
+                    objects_found.append(line.split(':')[1].lstrip(' ').rstrip(' '))
+            self.objects[obj] = objects_found
         return
+    
+    def cmd_select_object_type(self,event):
+        obj_type = self.cbox_object_type.get()
+        self.cbox_object_name.config(values=self.objects[obj_type])
+        self.cbox_object_name.set(self.objects[obj_type][0])
+        
     
     def cmd_escape(self,event):
         self.frame_search.pack_forget()
