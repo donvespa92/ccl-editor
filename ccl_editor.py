9a7fcfa3c8804b69ad0f829b69989be6cfdee580
Bence Darázs <bence.darazs@econ.local>
2018-01-12 15:39:28 +0100

    Search and replace implemented, save is still not

--- ccl_editor.py
+++ ccl_editor.py
@@ -15,13 +15,7 @@ class MainApplication(tk.Frame):
         self.master = master
         self.master.bind('<Escape>',self.cmd_escape)
         self.master.bind('<Control-f>',self.cmd_search)
-        self.object_types = ['Domain',
-                             'Boundary',
-                             'Domain Interface',
-                             'Material',
-                             'Expressions',
-                             'Monitor Point',
-                             ]
+        self.object_types = []
         self.object_dict = {'Domain':2,
                             'Material':2,
                             'Boundary':4,
@@ -60,21 +54,34 @@ class MainApplication(tk.Frame):
         self.text_output.config(yscrollcommand=self.text_yscrollbar.set)
      
     def gui_set_searchbar(self):
-        self.entry_searchbar = tk.Entry(self.frame_search,font=self.font,width=50)
-     
+        self.label_searchbar = tk.Label(self.frame_search,text='Search')
+        self.entry_searchbar = tk.Entry(self.frame_search,width=50)
+        self.entry_searchbar.bind("<KeyRelease>", self.search_text)
+        self.entry_searchbar.bind("<Return>", self.search_next)
+        
+        self.var_replace = tk.IntVar()
+        self.checkbutton_replace = tk.Checkbutton(
+                self.frame_search,
+                text='Replace',
+                command=self.cmd_replace,
+                variable=self.var_replace)
+        
+        self.label_replace = tk.Label(self.frame_search,text='Replace')
+        self.entry_replace = tk.Entry(self.frame_search,width=50,state='disabled')
+        self.entry_replace.bind("<Return>", self.replace_string)
+    
     def gui_set_combobox(self):
         self.label_object_type = tk.Label(self.mainframe,text='Object type')
         self.cbox_object_type = ttk.Combobox(
                 self.mainframe,
                 values=self.object_types,
-                state='readonly')
+                state='disabled')
         self.label_object_name = tk.Label(self.mainframe,text='Object name')
         self.cbox_object_name = ttk.Combobox(
                 self.mainframe,
                 values=self.selected_objects,
-                state='readonly')
+                state='disabled')
         self.cbox_object_type.bind("<<ComboboxSelected>>", self.cmd_select_object_type)
-        self.cbox_object_type.set(self.object_types[0])
         self.cbox_object_name.bind("<<ComboboxSelected>>", self.cmd_select_object_name)
    
     def gui_set_grid(self):
@@ -126,10 +133,13 @@ class MainApplication(tk.Frame):
             self.inputfile_type = os.path.splitext(self.inputfile_fullpath)[1]
             
             self.get_obj_data()
+            
         else:
             return
                
     def get_obj_data(self):
+        self.cbox_object_name.config(state='readonly')
+        self.cbox_object_type.config(state='readonly')
         if os.path.exists('temp.ccl'):
             os.remove('temp.ccl')
         if self.inputfile_type == '.ccl':
@@ -151,7 +161,7 @@ class MainApplication(tk.Frame):
             objects_found.append('All')
             if obj == 'Boundary':
                 for line in self.orig_setup:
-                    if (obj.upper()+':' in line and ' Side ' not in line):
+                    if (obj.upper()+':' in line):
                         objects_found.append(line.split(':')[1].lstrip(' ').rstrip(' '))
             elif obj == 'Expressions':
                 temp_data = []
@@ -171,52 +181,87 @@ class MainApplication(tk.Frame):
                     if obj.upper()+':' in line:
                         objects_found.append(line.split(':')[1].lstrip(' ').rstrip(' '))
             self.objects[obj] = objects_found
+            if len(objects_found) != 1:
+                self.object_types.append(obj)
+        
+        self.cbox_object_type.config(values=self.object_types)
         return
     
     def cmd_select_object_name(self,event):
+        self.text_output.config(state='normal')
+        self.text_output.delete(1.0,'end')
+        self.text_output.config(state='disabled')
+        
         self.selected_object = []
         self.selected_object_data = []
         obj_type = self.cbox_object_type.get()
         obj_name = self.cbox_object_name.get()
         
+        self.fidx = 0
+        self.lidx = 0
         temp_data = []
-        
         if obj_type == 'Expressions':
-            for idx,line in enumerate(self.orig_setup):
-                if obj_name in line and ' = ' in line:
-                    temp_data = self.orig_setup[idx:]
-                    break
-            for idx,line in enumerate(temp_data):
-                if ' = ' in line and not re.search(r'\\$',line):
-                    self.selected_object_data = temp_data[:idx+1]
-                    break
-                elif ' = ' in line and re.search(r'\\$',line):
-                    self.selected_object_data.append(line)
-                    temp_data = temp_data[idx+1:]
-                    break
-            for line in temp_data:
-                if ' = ' in line:
-                    break
-                else:
-                    self.selected_object_data.append(line)                 
+            if obj_name == 'All':
+                for idx,line in enumerate(self.orig_setup):
+                    if obj_type.upper()+':' in line:
+                        temp_data = self.orig_setup[idx:]
+                        self.fidx = idx
+                        break
+                for idx,line in enumerate(temp_data):
+                    if re.search(r'^\s{%s}END' % self.object_dict[obj_type],line):
+                        self.selected_object_data = temp_data[:idx+1]
+                        self.lidx = idx
+                        break
+            else:
+                for idx,line in enumerate(self.orig_setup):
+                    if obj_name in line and ' = ' in line:
+                        temp_data = self.orig_setup[idx:]
+                        self.fidx = idx
+                        break
+                for idx,line in enumerate(temp_data):
+                    if ' = ' in line and not re.search(r'\\$',line):
+                        self.selected_object_data = temp_data[:idx+1]
+                        self.fidx = idx
+                        self.lidx = idx
+                        break
+                    elif ' = ' in line and re.search(r'\\$',line):
+                        self.selected_object_data.append(line)
+                        temp_data = temp_data[idx+1:]
+                        self.fidx = idx
+                        break
+                if temp_data:
+                    for idx,line in enumerate(temp_data):
+                        if ' = ' in line:
+                            self.lidx = idx
+                            break
+                        else:
+                            self.selected_object_data.append(line)                 
         elif obj_name == 'All':
             self.selected_object_data = self.get_selected_obj_data(obj_type)
         else:
             for idx,line in enumerate(self.orig_setup):
                 if obj_type.upper() in line and obj_name in line:
                     temp_data = self.orig_setup[idx:]
+                    self.fidx = idx
                     break
             for idx,line in enumerate(temp_data):
                 if re.search(r'^\s{%s}END' % self.object_dict[obj_type],line):
                     self.selected_object_data = temp_data[:idx+1]
+                    self.lidx = idx
                     break
         
         self.update_text(self.selected_object_data)
-    
+        self.highlight_block('dom','DOMAIN:',2,'red')
+        self.highlight_block('bnd','BOUNDARY:',4,'blue')
+        self.highlight_block('if','DOMAIN INTERFACE:',2,'green')
+        self.highlight_block('material','MATERIAL:',2,'orange')
+        
+        
     def get_selected_obj_data(self,obj_type):
         selected = []
         temp_data = []
         fidx = []
+        lidx = []
         for idx,line in enumerate(self.orig_setup):
             if obj_type == 'Boundary':
                 if re.search(r'(%s:)' % obj_type.upper(),line) and 'Side' not in line:
@@ -224,13 +269,16 @@ class MainApplication(tk.Frame):
             else:
                 if re.search(r'(%s:)' % obj_type.upper(),line):
                     fidx.append(idx)
-                    
+        
+        self.fidx = fidx[0]         
         for findex in fidx:
             temp_data = self.orig_setup[findex:]
             for idx,line in enumerate(temp_data): 
                 if re.search(r'^\s{%s}END' % self.object_dict[obj_type],line):
                     selected = selected + temp_data[:idx+1]
+                    lidx.append(idx)
                     break
+        self.lidx = lidx[-1]
         return selected
             
             
@@ -242,20 +290,141 @@ class MainApplication(tk.Frame):
         self.text_output.config(state='disabled')    
     
     def cmd_select_object_type(self,event):
+        self.text_output.config(state='normal')
+        self.text_output.delete(1.0,'end')
+        self.text_output.config(state='disabled')
+        
         obj_type = self.cbox_object_type.get()
         self.cbox_object_name.config(values=self.objects[obj_type])
         self.cbox_object_name.set(self.objects[obj_type][0])
         
     
     def cmd_escape(self,event):
+        self.checkbutton_replace.deselect()
+        self.entry_replace.delete(0,'end')
+        self.entry_replace.config(state='disabled')
+        self.entry_searchbar.delete(0,'end')
         self.frame_search.pack_forget()
+        self.text_output.tag_remove('found', '1.0', 'end')
         return
 
     def cmd_search(self,*arg):
         self.frame_search.pack(fill='both',expand=1,padx=5,pady=5)
-        self.entry_searchbar.pack(fill='both',expand=1,padx=5,pady=5)
+        self.label_searchbar.grid(row=1,column=1,sticky='E')
+        self.entry_searchbar.grid(row=1,column=2)
+        self.checkbutton_replace.grid(row=2,column=1,sticky='EW')
+        self.entry_replace.grid(row=2,column=2)
         self.entry_searchbar.focus_set()
         return
+    
+    def cmd_replace(self):
+        self.entry_replace.config(state='normal')
+    
+    def replace_string(self,event):
+        str_old = self.entry_searchbar.get()
+        str_new = self.entry_replace.get()
+        
+        data_new = self.selected_object_data
+        for idx,line in enumerate(data_new):
+            if str_old in line:
+                data_new[idx] = line.replace(str_old,str_new)
+        
+        new_setup = self.orig_setup
+        new_setup[self.fidx:self.lidx] = data_new
+        return
+ 
+    def highlight_block(self,tagname,string,spaces,color):
+        objects_found = False
+        idx = '1.0'
+        self.text_output.tag_remove(tagname, '1.0', 'end')
+        self.text_output.tag_remove('obj_name', '1.0', 'end')
+        while 1: 
+            idx = self.text_output.search(string, idx, nocase=1, stopindex='end')
+            if not idx: break
+            lidx = '%s+%dc' % (idx, len(string))
+            self.text_output.tag_add(tagname, idx, lidx)
+            self.text_output.tag_add(
+                    'obj_name',
+                    lidx,
+                    self.text_output.index('%d.end' % float(idx)))
+            idx = lidx
+            objects_found = True
+            
+        self.text_output.tag_config(
+                tagname,
+                foreground=color,
+                font='Consolas 11 bold')
+        self.text_output.tag_config(
+                'obj_name',
+                font='Consolas 11 bold')
+        
+        if objects_found == True:
+            idx = '1.0'
+            self.text_output.tag_remove('end', '1.0', 'end')
+            while 1: 
+                idx = self.text_output.search(r'^\s{%s}END' % spaces, idx, nocase=1, stopindex='end',regexp=True)
+                if not idx: break
+                lidx = '%s+%dc' % (idx, len('END')+spaces)
+                self.text_output.tag_add('end', idx, lidx)
+                idx = lidx
+            
+            self.text_output.tag_config(
+                    'end',
+                    foreground=color,
+                    font='Consolas 11 bold')
+    
+    def highlight_names(self,tagname,obj_type):
+        self.text_output.tag_remove(tagname, '1.0', 'end')
+        for name in self.objects[obj_type]:
+            idx = '1.0'
+            while 1: 
+                idx = self.text_output.search(': '+name, idx, nocase=1, stopindex='end')
+                if not idx: break
+                lidx = '%s+%dc' % (idx, len(name)+2)
+                self.text_output.tag_add(tagname, idx, lidx)
+                idx = lidx
+            
+        self.text_output.tag_config(
+                tagname,
+                font='Consolas 11 bold')
+        
+    def search_text(self,event):
+        if event.keysym != 'Return':
+            self.counter = 1
+            self.text_output.tag_remove('found', '1.0', 'end')
+            tag = self.entry_searchbar.get()
+            self.fidx_search = []
+            if tag:
+                idx = '1.0'
+                while 1:
+                    idx = self.text_output.search(r'(%s)' % tag, idx, nocase=0, stopindex='end',regexp=True)
+                    if not idx: break
+                    self.fidx_search.append(math.floor(float(idx)))
+                    lastidx = '%s+%dc' % (idx, len(tag))
+                    self.text_output.tag_add('found', idx, lastidx)
+                    idx = lastidx
+                self.text_output.tag_config(
+                        'found',
+                        foreground='red',
+                        background='yellow',
+                        font='Consolas 11 bold')
+            else:
+                self.text_output.yview_moveto(0)
+            
+            if self.fidx_search:
+                self.text_output.yview_moveto(0)
+                self.text_output.yview_scroll(self.fidx_search[0]-1,'units')
+        else:
+            return
+        
+    def search_next(self,event):
+        if self.fidx_search:
+            self.text_output.yview_moveto(0)
+            self.text_output.yview_scroll(self.fidx_search[self.counter]-1,'units')
+        if self.counter == len(self.fidx)-1:
+            self.counter = 1
+        else:
+            self.counter += 1
     
 
 
